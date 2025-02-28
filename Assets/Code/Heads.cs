c607ddb638418dc7e7c86ecaea2799ab7991d755
CrypticOWO <zhao.ryan.77@gmail.com>
2025-02-28 02:25:57 -0500

    DONE

---
+++ Assets/Code/Heads.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+using TMPro;
+
+public class Heads : MonoBehaviour
+{
+    public Button Button;
+    public RawImage VideoDisplay;
+    public VideoPlayer VideoPlayer;
+    public static int Score = 0;
+    public TMP_Text ScoreText;
+
+    private void Start()
+    {
+        VideoDisplay.gameObject.SetActive(false);
+        VideoPlayer.gameObject.SetActive(false);
+        VideoPlayer.playOnAwake = false;
+        VideoPlayer.Stop();
+        ScoreText.text = "Correct Guesses:" + Score;
+    }
+
+    public void OnButtonClick()
+    {
+        VideoDisplay.gameObject.SetActive(true);
+        VideoPlayer.gameObject.SetActive(true);
+        VideoPlayer.Play();
+        Button.interactable = false;
+
+        Score++;
+
+        VideoPlayer.loopPointReached += OnVideoEnd;
+    }
+
+    void OnVideoEnd(VideoPlayer vp)
+    {
+        VideoDisplay.gameObject.SetActive(false);
+        VideoPlayer.gameObject.SetActive(false);
+        Button.interactable = true;
+
+        ScoreText.text = "Correct Guesses:" + Score;
+    }
+}
